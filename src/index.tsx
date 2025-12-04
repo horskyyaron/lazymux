c8a4ab3c08ad5e39da8b157b6977561ad94325ab
Yaron Horsky <horsky.yaron@gmail.com>
2025-12-04 20:19:41 +0200

    lazymux basic layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,26 @@
-import { createCliRenderer, type TextareaRenderable } from "@opentui/core";
-import { createRoot, useKeyboard, useRenderer } from "@opentui/react";
-import { useEffect, useRef } from "react";
+import { createCliRenderer } from "@opentui/core";
+import { createRoot } from "@opentui/react";
 
 function App() {
-  const renderer = useRenderer();
-  const textareaRef = useRef<TextareaRenderable>(null);
-
-  useEffect(() => {
-    renderer.console.show();
-  }, [renderer]);
-
-  useKeyboard((key) => {
-    if (key.name === "i") {
-      key.preventDefault();
-      textareaRef.current?.focus();
-      return;
-    }
-    if (key.name === "return") {
-      if (!textareaRef.current?.focused) return;
-      console.log(textareaRef.current?.plainText);
-      textareaRef.current?.blur();
-    }
-  });
-
   return (
-    <box title="Interactive Editor" style={{ border: true, flexGrow: 1 }}>
-      <textarea ref={textareaRef} placeholder="Type here..." focused />
+    <box
+      border
+      height={"100%"}
+      borderStyle="rounded"
+      title="lazymux"
+      titleAlignment="center"
+      flexDirection="row"
+    >
+      <box width={"30%"}>
+        <box border height={"50%"} title={"[1]-sessions"}></box>
+        <box border height={"50%"} title={"[2]-projects"}></box>
+      </box>
+      <box
+        border
+        width={"70%"}
+        title={"project's readme"}
+        titleAlignment="center"
+      ></box>
     </box>
   );
 }
