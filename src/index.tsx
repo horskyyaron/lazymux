3bc0ba6961963d0b7b2c077a7c0519af93552598
Yaron Horsky <horsky.yaron@gmail.com>
2025-12-04 16:54:13 +0200

    first

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,55 @@
-import { createCliRenderer, TextAttributes } from "@opentui/core";
-import { createRoot } from "@opentui/react";
+import { createCliRenderer } from "@opentui/core";
+import {
+  createRoot,
+  useKeyboard,
+  useRenderer,
+  useOnResize,
+  useTerminalDimensions,
+} from "@opentui/react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const { height, width } = useTerminalDimensions();
+  const [pressedKeys, setPressedKeys] = useState<Set<string>>(new Set());
+
+  const renderer = useRenderer();
+  useKeyboard(
+    (event) => {
+      setPressedKeys((keys) => {
+        const newKeys = new Set(keys);
+        if (event.eventType === "release") {
+          newKeys.delete(event.name);
+        } else {
+          newKeys.add(event.name);
+        }
+        return newKeys;
+      });
+    },
+    { release: true },
+  );
+
+  useEffect(() => {
+    renderer.console.show();
+    console.log("hello");
+  }, []);
+
+  useOnResize((width, height) => {
+    console.log(`Terminal resized to ${width}x${height}`);
+  });
+
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
+    <box>
+      <text>
+        terminal dimensions: {width} : {height}
+      </text>
+      <box
+        style={{
+          width: Math.floor(width / 2),
+          height: Math.floor(height / 3),
+          border: true,
+        }}
+      >
+        <text>1/2 width, 1/3 size</text>
       </box>
     </box>
   );
