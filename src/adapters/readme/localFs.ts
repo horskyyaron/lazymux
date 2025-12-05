fd8e3085c3a2e3a5befd6b000f32712900228bef
Yaron Horsky <horsky.yaron@gmail.com>
2025-12-05 23:44:56 +0200

    reading local readme file

--- src/adapters/readme/localFs.ts
+++ src/adapters/readme/localFs.ts
@@ -3,9 +3,16 @@ import { promises as fs } from "fs";
 import path from "path";
 import type { Project, Session } from "../../data/types";
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const possibleNames = ["README.md", "readme.md", "Readme.md", "README"];
 
 export async function getReadme(source: Project | Session): Promise<string> {
-  await sleep(150);
-  return `# ${source.name}\n\n* some text\n## Hello`;
+  console.log("in  reademe local", source);
+  for (const name of possibleNames) {
+    const full = path.join(source.path, name);
+    try {
+      return await fs.readFile(full, "utf8");
+    } catch {}
+  }
+
+  return `# ${source.name}\n\n_No README found in this project._`;
 }
