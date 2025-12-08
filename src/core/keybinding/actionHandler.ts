6dfacfe93c3d86bee2031a89656f9c895d9b6390
Yaron Horsky <horsky.yaron@gmail.com>
2025-12-08 10:22:52 +0200

    created a actionhandler for starting a session

--- src/core/keybinding/actionHandler.ts
+++ src/core/keybinding/actionHandler.ts
@@ -1,22 +1,23 @@
 import {
+  createTmuxSession,
   killTmuxSession,
   switchTmuxSession,
 } from "../../adapters/multiplexer/tmux";
 import { Action } from "./keybinding";
 
-type ActionHandler = (payload: { name: string }) => Promise<boolean>;
+type ActionHandler = (payload: {
+  name: string;
+  path?: string;
+}) => Promise<boolean>;
 
 export const actionHandlers: Record<Action, ActionHandler> = {
-  [Action.KILL_SESSION]: async ({ name }) => killTmuxSession(name),
-  [Action.SWITCH_SESSION_CLIENT]: async ({ name }) => switchTmuxSession(name),
-  [Action.RENAME_SESSION]: async ({ name }) => {
-    // placeholder
-    console.log("rename session not implemented");
-    return false;
-  },
-  [Action.START_PROJECT_SESSION]: async ({ name }) => {
-    console.log("start project session", name);
-    return false;
+  [Action.KILL_SESSION]: async ({ name }) => await killTmuxSession(name),
+  [Action.SWITCH_SESSION_CLIENT]: async ({ name }) =>
+    await switchTmuxSession(name),
+  [Action.START_PROJECT_SESSION]: async ({ name, path }) => {
+    if (!path)
+      throw new Error("must provide a path for starting a new session");
+    return await createTmuxSession(name, path);
   },
   [Action.OPEN_PROJECT_FOLDER]: async ({ name }) => {
     console.log("open folder", name);
@@ -26,8 +27,19 @@ export const actionHandlers: Record<Action, ActionHandler> = {
     console.log("delete folder", name);
     return false;
   },
-  [Action.FOCUS_ON_README]: async ({ name }) => {
-    console.log("focus on readme", name);
+  [Action.FOCUS_ON_README]: function (payload: {
+    name: string;
+  }): Promise<boolean> {
+    throw new Error("Function not implemented.");
+  },
+  [Action.PREVIOUS_TAB]: function (payload: {
+    name: string;
+  }): Promise<boolean> {
+    throw new Error("Function not implemented.");
+  },
+  [Action.RENAME_SESSION]: async ({ name }) => {
+    // placeholder
+    console.log("rename session not implemented");
     return false;
   },
 };
